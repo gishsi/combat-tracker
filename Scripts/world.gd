239131dd5c4584f1cc2a8cc9e5236c40ad4abfc3
gishsi <spokosalo@gmail.com>
2026-04-27 09:22:34 +0100

    inidicate mode on ui

--- Scripts/world.gd
+++ Scripts/world.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+@onready var modeIndicatorRect = $ColorRect
+
+func _on_add_tokens_button_pressed() -> void:
+	modeIndicatorRect.color = Color.CORNFLOWER_BLUE
+
+func _on_move_tokens_button_pressed() -> void:
+	modeIndicatorRect.color = Color.MEDIUM_SEA_GREEN
